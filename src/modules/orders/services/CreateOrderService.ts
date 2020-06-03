8c8a422dc2a876ca55615e27ee8afb33821e7192
Samuel Terra <samuelterra22@hotmail.com>
2020-06-02 22:35:16 -0300

    Implement challenge

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,48 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) throw new AppError('Customer does not exists.');
+
+    const productsIds = products.map(product => {
+      return { id: product.id };
+    });
+
+    const productData = await this.productsRepository.findAllById(productsIds);
+
+    if (productData.length !== products.length) {
+      throw new AppError('Missing products.');
+    }
+
+    const productsFinal = productData.map(data => {
+      const productFinal = products.find(product => product.id === data.id);
+      return {
+        product_id: data.id,
+        price: data.price,
+        quantity: productFinal?.quantity || 0,
+      };
+    });
+
+    const order = this.ordersRepository.create({
+      customer,
+      products: productsFinal,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1591144811036-CreteOrdersProducts.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreteOrdersProducts1591144811036
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+          {
+            name: 'price',
+            type: 'numeric',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'foreign_order_fk',
+            columnNames: ['order_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orders',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'foreign_product_fk',
+            columnNames: ['product_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
